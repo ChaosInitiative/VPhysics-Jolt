dfa22077a40c43fc6d2190e6f89ff47f1cb80f73
SCell555 <kubci.rusnk645@gmail.com>
2023-06-11 13:51:21 +0200

    feat: Hook up pulley, not tested

--- vphysics_jolt/vjolt_controller_vehicle.cpp
+++ vphysics_jolt/vjolt_controller_vehicle.cpp
@@ -408,7 +408,7 @@ void JoltPhysicsVehicleController::CreateWheel( JPH::VehicleConstraintSettings &
 
 	JPH::WheelSettingsWV *wheelSettings = new JPH::WheelSettingsWV;
 	wheelSettings->mPosition			= SourceToJolt::Distance( wheelPositionLocal );
-	wheelSettings->mDirection			= JPH::Vec3( 0, 0, -1 );
+	wheelSettings->mSuspensionDirection	= JPH::Vec3( 0, 0, -1 );
 	wheelSettings->mAngularDamping		= axle.wheels.rotdamping;
 	// TODO(Josh): What about more than 4 wheels?
 	wheelSettings->mMaxSteerAngle		= axleIdx == 0 ? steeringAngle : 0.0f;
